dfb2d93650219bc659ea3a2b08badcda2ae21dea
Shannon Griffin <76169649+Hairbomination@users.noreply.github.com>
2020-12-28 17:27:25 +1100

    Add files via upload

---
+++ Ch6Example1/Ch6Example1.cpp
@@ -0,0 +1,18 @@
+#include "constants.h"
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "How many students are in your class? ";
+	int students{};
+	std::cin >> students;
+
+
+	if (students > constants::max_class_size)
+		std::cout << "There are too many students in this class";
+	else
+		std::cout << "This class isn't too large";
+
+	return 0;
+}
---
+++ Ch6Example1/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants
+{
+	inline constexpr int max_class_size{ 20 };
+}
+#endif
