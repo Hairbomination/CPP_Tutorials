bd69d18f2d1adf3460ae608b7630f63469c4bc7e
Shannon Griffin <76169649+Hairbomination@users.noreply.github.com>
2020-12-28 17:02:05 +1100

    Add files via upload

---
+++ Ch6Example2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+bool passOrFail()
+{
+	static int s_x{};
+	++s_x;
+	
+	return (s_x < 4);
+}
+
+int main()
+{
+	std::cout << "User #1: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #2: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #3: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #4: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+	std::cout << "User #5: " << (passOrFail() ? "Pass" : "Fail") << '\n';
+
+	return 0;
+}
