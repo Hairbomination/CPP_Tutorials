33e891f6da33f81c78c38d07020affdb6dfea617
Scott Lorberbaum <scott@laureltreecomputing.com>
2021-06-15 13:04:57 -0600

    made more changes a few macro definitions to clean up code, can this be done with using clauses instead? - whichRank = static_cast<Rank> - whichSuit = static_cast<Suit> - castToInt = static_cast<std::size_t> --> currently instead of int
    static constexpr of ranks_per_suit and suits_per_deck now
    
    createDeck()
    - use of new macros
    - instead of hardcoded modulo numbers, now they are from variables

--- Ch9QuizQ6d.cpp
+++ Ch9QuizQ6d.cpp
@@ -3,6 +3,10 @@
 #include<cstddef>
 #include<iterator>
 
+#define whichRank static_cast<Rank>
+#define whichSuit static_cast<Suit>
+#define castToInt static_cast<std::size_t>
+
 enum class Rank
 {
 	two,
@@ -21,7 +25,10 @@ enum class Rank
 	number_of_ranks_in_suit
 };
 
-static const std::array<std::string, static_cast<int>(Rank::number_of_ranks_in_suit)> rank_names =  {
+
+static constexpr std::size_t ranks_per_suit = castToInt(Rank::number_of_ranks_in_suit);
+
+static const std::array<std::string, ranks_per_suit> rank_names =  {
 	"2",
 	"3",
 	"4",
@@ -46,7 +53,9 @@ enum class Suit
 	number_of_suits_in_deck
 };
 
-static  const std::array<std::string, static_cast<int>(Suit::number_of_suits_in_deck)> suit_names = {
+static constexpr std::size_t suits_per_deck = castToInt(Suit::number_of_suits_in_deck);
+
+static  const std::array<std::string, suits_per_deck> suit_names = {
 	"H", //Hearts
 	"D", //Diamonds
 	"S", //Spades
@@ -58,26 +67,26 @@ struct Card
 	Rank rank{};
 	Suit suit{};
 };
-static constexpr std::size_t deck_size = static_cast<std::size_t>(Rank::number_of_ranks_in_suit)*static_cast<std::size_t>(Suit::number_of_suits_in_deck);
+static constexpr std::size_t deck_size =suits_per_deck * ranks_per_suit;
 
 using array_t = std::array<Card, deck_size>;
 
 void createDeck(array_t& deck)
 {
 
-	Rank currentRank{static_cast<Rank>(0)};
-	Suit currentSuit{static_cast<Suit>(0)};
+	Rank currentRank{ whichRank(0) };
+	Suit currentSuit{ whichSuit(0) };
 
 	for (auto& card: deck )
 	{
 		card.suit = currentSuit;
 		card.rank = currentRank;
-		currentRank = static_cast<Rank>(static_cast<int>(currentRank) + 1);
-		currentRank = static_cast<Rank>(static_cast<int>(currentRank) % 13);
-		if(currentRank == static_cast<Rank>(0))
+		currentRank = whichRank(castToInt(currentRank) + 1);
+		currentRank = whichRank(castToInt(currentRank) % ranks_per_suit);
+		if(currentRank == whichRank(0))
 		{
-			currentSuit = static_cast<Suit>(static_cast<int>(currentSuit) + 1);
-			currentSuit = static_cast<Suit>(static_cast<int>(currentSuit) % 4);
+			currentSuit = whichSuit(castToInt(currentSuit) + 1);
+			currentSuit = whichSuit(castToInt(currentSuit) % suits_per_deck);
 		}
 	}
 }
@@ -86,7 +95,7 @@ void printDeck(const array_t& deck)
 {
 	for (auto card : deck)
 	{
-		std::cout << suit_names[static_cast<std::size_t>(card.suit)] << rank_names[static_cast<std::size_t>(card.rank)] << " ";
+		std::cout << suit_names[castToInt(card.suit)] << rank_names[castToInt(card.rank)] << " ";
 	}
 }
 
