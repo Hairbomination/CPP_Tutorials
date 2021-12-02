8d47f3b682f792366024b3060b24191d3b2aedb3
Shannon Griffin <76169649+Hairbomination@users.noreply.github.com>
2021-12-02 18:29:49 +1100

    Add files via upload

---
+++ Ch12.15Q1c/Ch12.15Q1c.cpp
@@ -0,0 +1,15 @@
+#include "Point3d.h"
+#include "Vector3d.h"
+#include <iostream>		//Not needed because no print statement.
+
+int main()
+{
+	Point3d p{ 1.0, 2.0, 3.0 };
+	Vector3d v{ 2.0, 2.0, -3.0 };
+
+	p.print();
+	p.moveByVector(v);
+	p.print();
+
+	return 0;
+}
